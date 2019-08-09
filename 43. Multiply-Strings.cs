947b640573cbd2c4e9555ed8cf5c5ec4eed31e6d
Gus Yang <gusyang@users.noreply.github.com>
2019-08-09 11:57:34 -0700

    update

--- 43. Multiply-Strings.cs
+++ 43. Multiply-Strings.cs
@@ -1,3 +1,19 @@
+/*
+Example 1:
+
+Input: num1 = "2", num2 = "3"
+Output: "6"
+Example 2:
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+Note:
+
+The length of both num1 and num2 is < 110.
+Both num1 and num2 contain only digits 0-9.
+Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+*/
 public class Solution
 {
     public string Multiply(string num1, string num2)
