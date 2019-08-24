3fdfe7f6c8c4e94c7f98972c7d252a322e16401b
gusyang <gus.yang@aol.com>
2019-08-23 22:19:42 -0700

    update add 2 number

--- 2.add-two-numbers.cs
+++ 2.add-two-numbers.cs
@@ -13,7 +13,29 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        
+        if(l1 == null && l2 == null){
+            return null;
+        }
+        int sum = 0, carry = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null || l2 != null || carry == 1)
+        {
+            sum = carry + (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+            carry = sum > 9 ? 1 : 0;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+        if(carry != 0){
+            curr.next = new ListNode(carry);
+        }
+        return dummy.next;
     }
 }
 
