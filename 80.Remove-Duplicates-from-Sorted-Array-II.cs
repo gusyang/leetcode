493f4816360432a8d7b13b479fa0b1c560df1fbd
gusyang <gus.yang@aol.com>
2019-08-25 20:02:03 -0700

    80 remove duplicate

---
+++ 80.Remove-Duplicates-from-Sorted-Array-II.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+Given nums = [1,1,1,2,2,3],
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,1,2,3,3],
+Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+It doesn't matter what values are set beyond the returned length.
+*/
+namespace ConsoleApp1
+{
+    public static class _80
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+            int res = 1;
+            int dup = 1;
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if(nums[i] == nums[i - 1])
+                {
+                    if(dup > 2)
+                    {
+                        continue;
+                    }
+                    nums[res] = nums[i];
+                    res++;
+                    dup++;
+                }
+                else
+                {
+                    nums[res] = nums[i];
+                    res++;
+                    dup = 1;
+                }
+            }
+            return res + 1;
+        }
+    }
+}
