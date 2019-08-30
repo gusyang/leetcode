216307d48fe383276922ebdc3fe5b226be0e9694
Gus Yang <gusyang@users.noreply.github.com>
2019-08-30 12:58:30 -0700

    Add files via upload

---
+++ 89.Gray-Code.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/* 
+ *  Gray Code
+The gray code is a binary numeral system where two successive values differ in only one bit.
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+
+Note:
+For a given n, a gray code sequence is not uniquely defined.
+For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.*/
+namespace leetcode
+{
+    public static class _89
+    {
+        public static List<int> greycode(int n)
+        {
+            List<int> res = new List<int>();
+            
+            for(int i = 0; i < Math.Pow(2, n); i++)
+            {
+                res.Add((i >> 1) ^ i);
+            }
+            return res;
+        }
+    }
+}
