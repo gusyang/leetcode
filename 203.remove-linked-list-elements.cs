a455e12ebf1c399992b5fdfa0b903737af2570ca
gusyang <gus.yang@aol.com>
2019-09-04 21:16:23 -0700

    203

---
+++ 203.remove-linked-list-elements.cs
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=203 lang=csharp
+ *
+ * [203] Remove Linked List Elements
+ *
+ * https://leetcode.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (36.33%)
+ * Likes:    941
+ * Dislikes: 59
+ * Total Accepted:    246.6K
+ * Total Submissions: 678.4K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * Remove all elements from a linked list of integers that have value val.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val) {
+        
+    }
+}
+
