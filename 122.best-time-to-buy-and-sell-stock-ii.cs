958a2b37ae94a9abf9026dd10a5d9ea6076909fc
Gus Yang <gusyang@users.noreply.github.com>
2019-10-15 09:44:17 -0700

    Update 122.best-time-to-buy-and-sell-stock-ii.cs

--- 122.best-time-to-buy-and-sell-stock-ii.cs
+++ 122.best-time-to-buy-and-sell-stock-ii.cs
@@ -69,5 +69,15 @@ public class Solution {
 
         return maxprofit;
     }
+    
+     public int MaxProfit(int[] prices) {
+        int max = 0;
+        for(int i = 1; i < prices.Length; i++ ){
+            if(prices[i] > prices[i - 1]){
+                max += prices[i] - prices[i - 1];
+            }
+        }
+        return max;
+    }
 }
 
