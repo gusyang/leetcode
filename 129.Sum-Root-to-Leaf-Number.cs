f9b4e7f8f9818403d3ee5de7ac053912de6a518b
Gus Yang <gusyang@users.noreply.github.com>
2019-10-16 14:32:13 -0700

    129

---
+++ 129.Sum-Root-to-Leaf-Number.cs
@@ -0,0 +1,68 @@
+/*
+129. Sum Root to Leaf Numbers
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+Find the total sum of all root-to-leaf numbers.
+Note: A leaf is a node with no children.
+
+Example:
+
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+
+Example 2:
+
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+ */
+/**
+* Definition for a binary tree node.
+* public class TreeNode {
+*     public int val;
+*     public TreeNode left;
+*     public TreeNode right;
+*     public TreeNode(int x) { val = x; }
+* }
+*/
+namespace myapp
+{
+public class Solution
+{
+    public int SumNumbers(TreeNode root)
+    {
+        int total = 0;
+        Helper(root, 0, ref total);
+        return total;
+    }
+    private void Helper(TreeNode root, int curr, ref int total){
+        if(root == null){
+            return;
+        }
+        curr = curr * 10 + root.val;
+        if(root.left == null && root.right == null){
+            total += curr;
+            return;
+            }
+            Helper(root.left, curr, ref total);
+            Helper(root.right, curr, ref total);
+    }
+}
+}
